286279343566c9d3f0e0730fba8310e35f28f965
Run <48591593+papilong123@users.noreply.github.com>
2021-07-14 12:18:12 +0800

    创建构造函数

--- kafka/src/main/java/com/example/kafka/service/AdminClientCreateTopic.java
+++ kafka/src/main/java/com/example/kafka/service/AdminClientCreateTopic.java
@@ -1,8 +1,8 @@
 package com.example.kafka.service;
 
+import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -14,10 +14,10 @@ import java.util.Collection;
  * 代码中通过AdminClient创建Topic，项目一启动就创建，创建了AdminClient的bean实例
  */
 @Service
+@AllArgsConstructor
 public class AdminClientCreateTopic {
 
-    @Autowired
-    private KafkaProperties properties;
+    private final KafkaProperties properties;
 
     @Bean
     public void testCreateTopic(){
@@ -25,7 +25,7 @@ public class AdminClientCreateTopic {
         if(client!=null){
             try {
                 Collection<NewTopic> newTopics = new ArrayList<>(1);
-                newTopics.add(new NewTopic("topic-k2", 1, (short) 1));
+                newTopics.add(new NewTopic("topic-k5", 1, (short) 1));
                 client.createTopics(newTopics);
             }catch (Throwable e){
                 e.printStackTrace();
