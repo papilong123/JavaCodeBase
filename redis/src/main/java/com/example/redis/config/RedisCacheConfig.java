76739448e075ec2099ba767b3dccb666183713d5
Run <48591593+papilong123@users.noreply.github.com>
2021-07-18 12:10:51 +0800

    fix

--- redis/src/main/java/com/example/redis/config/RedisCacheConfig.java
+++ redis/src/main/java/com/example/redis/config/RedisCacheConfig.java
@@ -2,29 +2,27 @@ package com.example.redis.config;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import javax.crypto.KeyGenerator;
 import java.lang.reflect.Method;
 
 @Configuration
 @EnableCaching
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RedisCacheConfig {
 
     private volatile LettuceConnectionFactory lettuceConnectionFactory;
     private volatile RedisTemplate<String, String> redisTemplate;
     private volatile RedisCacheManager redisCacheManager;
-
-    public RedisCacheConfig(){
-        super();
-    }
 
     @Bean
     public KeyGenerator keyGenerator(){
